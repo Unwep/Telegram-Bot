05d7bae8974bc5d8057502686b74b8130c1a5f67
escoupe <169478467+Unwep@users.noreply.github.com>
2026-01-04 21:38:31 +0300

    Update keyboards.py

--- keyboards.py
+++ keyboards.py
@@ -1,1 +1,16 @@
+from aiogram.types import ReplyKeyboardMarkup, InlineKeyboardMarkup, InlineKeyboardButton, KeyboardButton
+from aiogram.utils.keyboard import InlineKeyboardBuilder
 
+
+main = InlineKeyboardMarkup(inline_keyboard=[
+    [InlineKeyboardButton(text="оставить заявку", callback_data='apply')]
+])
+
+
+async def get_url_open_chat(user_id):
+    builder = InlineKeyboardBuilder()
+    builder.button(
+        text='Перейти к диалогу',
+        url=f'tg://openmessage?user_id={user_id}'
+    )
+    return builder.as_markup()
